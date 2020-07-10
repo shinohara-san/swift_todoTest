1555e5441aa7efa0df5cbb68a8a8bfe536310c42
shinohara-san <someiyoshino0809@gmail.com>
2020-07-10 18:13:52 +0900

    first

---
+++ TodoTest/TaskViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TaskViewController.swift
+//  TodoTest
+//
+//  Created by Yuki Shinohara on 2020/06/06.
+//  Copyright © 2020 Yuki Shinohara. All rights reserved.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    
+    var task: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = task
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
+    }
+    
+
+    @objc func deleteTask(){
+//        let newCount = count - 1
+//        UserDefaults().setValue(newCount, forKey: "count")
+//        UserDefaults().setValue(nil, forKey: "task_\(currentPosition)")
+    }
+
+}
